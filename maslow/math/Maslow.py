a2edd4fcb544a6261cfcb5e3e60da0d422becdf8
Michael Wetter <MWetter@lbl.gov>
2024-07-05 15:43:10 -0700

    Implemented SAG calculation

--- maslow/math/Maslow.py
+++ maslow/math/Maslow.py
@@ -10,6 +10,13 @@ import pandas as pd
 
 class Maslow(object):
     """ Class that implements the energy security metrics.
+
+        Note that I, P, D, E, L, Sb and Sd must all have the same
+        number of energy carriers, and these must be in the same order.
+        For example, if the first column of I is electricity, then the first
+        column of P must also be electricity, as these energy carriers
+        are added, subtracted or compared for each carrier type.
+
     """
 
     def __init__(self, filename, sheet, time,
@@ -21,7 +28,8 @@ class Maslow(object):
                  Sb,
                  Sd,
                  Sto_start,
-                 Sto_end):
+                 Sto_end,
+                 c):
 
         # --------------------------
         # Class variables
@@ -35,13 +43,41 @@ class Maslow(object):
         self._time_step = time[1] - time[0]
         self._I = self._get_pandas_frames(I)
         #FIXME
-        self._P = self._I
-        self._D = self._I
-        self._E = self._I
-        self._L = self._I
-        self._Sb = self._I
-        self._Sd = self._I
-        self._Sto_end = [0]
+        self._P = self._get_pandas_frames(P)
+        self._D = self._get_pandas_frames(D)
+        self._E = self._get_pandas_frames(E)
+        self._L = self._get_pandas_frames(L)
+        self._Sb = self._get_pandas_frames(Sb)
+        self._Sd = self._get_pandas_frames(Sd)
+        self._Sto_start = self._get_pandas_frames(Sto_start)
+        self._Sto_end = self._get_pandas_frames(Sto_end)
+        self._c = self._get_pandas_frames(c)
+
+        if self._I.shape != self._P.shape:
+            raise RuntimeError("I and P must have the same dimensions.")
+        if self._I.shape != self._D.shape:
+            raise RuntimeError("I and D must have the same dimensions.")
+        if self._I.shape != self._E.shape:
+            raise RuntimeError("I and E must have the same dimensions.")
+        if self._I.shape != self._L.shape:
+            raise RuntimeError("I and L must have the same dimensions.")
+        if self._I.shape != self._Sb.shape:
+            raise RuntimeError("I and Sb must have the same dimensions.")
+        if self._I.shape != self._Sd.shape:
+            raise RuntimeError("I and Sd must have the same dimensions.")
+        if self._I.shape[1] != self._Sto_start.shape[1]:
+            raise RuntimeError("I and Sto_start must have the same number of energy flows.")
+        if self._I.shape[1] != self._Sto_end.shape[1]:
+            raise RuntimeError("I and Sto_end must have the same number of energy flows.")
+        if self._I.shape[1] != self._c.shape[1]:
+            raise RuntimeError("I and c must have the same number of energy flows.")
+        if self._Sto_start.shape[0] != 1:
+            raise RuntimeError("Sto_start must have one row only as it is not a time series.")
+        if self._Sto_end.shape[0] != 1:
+            raise RuntimeError("Sto_end must have one row only as it is not a time series.")
+        if self._c.shape[0] != 1:
+            raise RuntimeError("c must have one row only as it is not a time series.")
+
         #FIXME: Do we need a Sto_start somewhere?
 
     def _get_pandas_frames(self, range_string):
@@ -54,9 +90,7 @@ class Maslow(object):
         for rs in range_string.split(","):
             fr=self._get_pandas_frame(rs)
             frames.append(fr)
-       # print(frames)
         frame = pd.concat(frames, axis=1)
-        print(frame)
         return frame
 
     def _get_pandas_frame(self, range_string):
@@ -110,16 +144,17 @@ class Maslow(object):
             integrals = Maslow.integrate_by_columns(P, self._time_step)
             denominator = sum(integrals)
             phi = list(np.multiply(1/denominator, integrals))
+            for p in phi:
+                if abs(1-p) < 1E-8:
+                    raise RuntimeError("Data error. Calculated phi for a quantity that is zero. This is not allowed for calculating SPG.")
             return phi
         
         def get_d(P):
             phis = get_phis(P)
             n = len(phis)
-            print(f"*** n = {n}")
             summand = 0
             for phi in phis:
                 summand += phi * np.log(phi)
-            print(f"*** summand = {summand}")
 
             d = - summand / np.log(n)
             return d
@@ -128,12 +163,60 @@ class Maslow(object):
 
         P = Maslow.integrate_all_columns(self._P, self._time_step)
         L = Maslow.integrate_all_columns(self._L, self._time_step)
-        Sto_end = sum(self._Sto_end)
+        # Call integrate with a time step of 1, which will get the right data format.
+        Sto_end = Maslow.integrate_all_columns(self._Sto_end, 1)
         D = Maslow.integrate_all_columns(self._D, self._time_step)
 
         SPG = get_d(self._P) * (P-L+Sto_end)/D
-        print(f"**** d = {get_d(self._P)}")
         return SPG
+
+    def get_SAG(self):
+        """
+        Returns the adequacy SAG
+        """
+        def get_f_i():
+            P = self._P.to_numpy()
+            I = self._I.to_numpy()
+            Sd = self._Sd.to_numpy()
+            Sb = self._Sb.to_numpy()
+            L = self._L.to_numpy()
+            D = self._D.to_numpy()
+
+            lhs = P + I + Sd - Sb - L
+            rhs = D
+
+            retB = (lhs >= rhs)
+            ret = retB.astype(float)
+
+            return ret
+
+        f = get_f_i()
+
+        D = self._D.to_numpy()
+        nFlo = D.shape[1]
+        sum_D_over_j = np.zeros(D.shape[0])
+        for i in range(nFlo):
+            sum_D_over_j += D[:,i]
+        # Compute integral, for each energy carrier i
+        num = f * D
+        # Array with each element being an energy carrier i, and the array contains the integral
+        a = np.zeros(nFlo)
+        for i in range(nFlo):
+            # Integrate over each time step and divide each term by sum_j D_j(t)
+            a1 = num[:,i] / sum_D_over_j
+            a[i] = sum(a1)
+
+        # sum of all c
+        c = self._c.to_numpy()
+        sum_c = 0
+        for i in range(nFlo):
+            sum_c += c[0,i]
+        summand = c / sum_c * a
+        SAG = 0
+        for i in range(nFlo):
+            SAG += summand[0, i]
+
+        return SAG
 
 
 
--- maslow/tests/test_math_Maslow.py
+++ maslow/tests/test_math_Maslow.py
@@ -13,20 +13,24 @@ class Test_Maslow(unittest.TestCase):
     def test_Maslow(self):
         import os
         import maslow.math.Maslow as m
-        c = m.Maslow(filename=os.path.join("maslow", "data", "Maslow_CH_Ref_exec.xlsx"),
+        c = m.Maslow(filename=os.path.join("maslow", "data", "smallTest.xlsx"),
                      sheet="supply_TS_smooth_Empa",
-                     time="A11:A8770",
-                     I="C11:C8770,D11:D8770",
-                     P="C11:C8770",
-                     D="C11:C8770",
-                     E="C11:C8770",
-                     L="C11:C8770",
-                     Sb="C11:C8770",
-                     Sd="C11:C8770",
-                     Sto_start = [0],
-                     Sto_end = [0])
+                     time="A11:A15",
+                     I="D11:D15,E11:E15",
+                     P="D11:D15,E11:E15",
+                     D="D11:D15,E11:E15",
+                     E="D11:D15,E11:E15",
+                     L="D11:D15,E11:E15",
+                     Sb="D11:D15,E11:E15",
+                     Sd="D11:D15,E11:E15",
+                     Sto_start = "D11:D11,E11:E11",
+                     Sto_end = "D11:D11,E11:E11",
+                     c = "D11:D11,E11:E11")
         SPG = c.get_SPG()
         print(f"*** SPG is {SPG}")
+        
+        SAG = c.get_SAG()
+        print(f"*** SAG is {SAG}")
 
         #c.get_I()
 
@@ -41,3 +45,5 @@ class Test_Maslow(unittest.TestCase):
         sum = m.Maslow.integrate_all_columns(df, 1)
         self.assertEqual(sum, 192)
 
+if __name__ == '__main__':
+    unittest.main()
